0f70c2818d310c4ea4c427fbd723753d5316f63a
Zac Monroe <zmonroe0820@gmail.com>
2019-01-29 14:40:45 -0500

    Updates

---
+++ precache-manifest.8eb9aca223c6f1c71065b6971386108a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bat/index.html/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2bbf2c4f09d7e1012ae2",
+    "url": "/bat/index.html/static/js/runtime~main.2bbf2c4f.js"
+  },
+  {
+    "revision": "694edcdbf612354cdae1",
+    "url": "/bat/index.html/static/js/main.694edcdb.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/bat/index.html/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "694edcdbf612354cdae1",
+    "url": "/bat/index.html/static/css/main.fd4a0ca5.chunk.css"
+  },
+  {
+    "revision": "f941e1ca79b86d80e07678c3bb5e362a",
+    "url": "/bat/index.html/index.html"
+  }
+];
